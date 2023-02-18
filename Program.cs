d016c9f80708019dbfdf6d4b1d0d6872d96c3558
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-02-18 21:36:39 +0300

    program.cs Added
    solition is in file.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        int count = 0;
+        int count2= 0;
+        int cur=6; //1 2 3 4 5 4 3 2 1 3 4
+      
+
+        for(int i=1;i<6;i++)
+        { 
+            foreach(int j in arr)
+            {
+                if (i == j)
+                {
+                    count++;
+                }
+            }
+            if (count > count2)
+            {
+                count2 = count;
+                cur = i;
+            }
+                
+            if (count == count2 && i < cur) cur = i;
+            count = 0;
+        }
+        return cur;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
